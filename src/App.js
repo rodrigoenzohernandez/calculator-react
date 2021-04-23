248539a7ca85191028fa7c4468c5340cd0596cd8
Rodrigo Hernandez <hernandez@eudaimonia.com.ar>
2021-04-22 21:01:25 -0300

    feat: add calculator structure in the same file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,37 @@ const App = () => {
   return (
   <main className='react-calculator'>
       Calculator
+
+      <div className="result">
+        </div>
+        <div className="numbers">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+        </div>
+        <div className="functions">
+            <button>
+                clear
+            </button>
+            <button>
+                r
+            </button>
+        </div>
+        <div className="math-operations">
+            <button>+</button>
+            <button>-</button>
+            <button>*</button>
+            <button>/</button>
+            <button>=</button>
+        </div>
+
   </main>)
 }
 
