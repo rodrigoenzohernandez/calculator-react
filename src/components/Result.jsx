de508dcca8285bc29548297148653d16f37606a9
Rodrigo Hernandez <hernandez@eudaimonia.com.ar>
2021-04-22 23:29:04 -0300

    feat: apply destructuring

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-const Result = (props) => {
+const Result = ({ value }) => {
     return (
         <div className="result">
-            {props.value}
+            {value}
         </div>
     )
 }
