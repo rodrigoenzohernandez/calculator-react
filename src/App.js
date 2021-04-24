ff18ef81a033d52648d2b48229445fc9a9d75735
Rodrigo Hernandez <hernandez@eudaimonia.com.ar>
2021-04-24 01:09:48 -0300

    feat: use component numbers

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@
 //Created components
 
 import Result from './components/Result'
-import Button from './components/Button/Button'
 import MathOperations from './components/MathOperations'
 import Functions from './components/Functions'
-
+import Numbers from './components/Numbers'
 //Style
 
 import './App.css';
 
 
 const App = () => {
-
-  //If the arrow function has just one parameter, is not needed the parenthesis
-  const clickHandlerFunction = text => console.log(text)
 
   //mathOperations function values
 
@@ -42,18 +38,7 @@ const App = () => {
 
       <Result value={"0"} />
 
-      <div className="numbers">
-        <Button text="1" clickHandler={clickHandlerFunction} />
-        <Button text="2" clickHandler={clickHandlerFunction} />
-        <Button text="3" clickHandler={clickHandlerFunction} />
-        <Button text="4" clickHandler={clickHandlerFunction} />
-        <Button text="5" clickHandler={clickHandlerFunction} />
-        <Button text="6" clickHandler={clickHandlerFunction} />
-        <Button text="7" clickHandler={clickHandlerFunction} />
-        <Button text="8" clickHandler={clickHandlerFunction} />
-        <Button text="9" clickHandler={clickHandlerFunction} />
-        <Button text="0" clickHandler={clickHandlerFunction} />
-      </div>
+      <Numbers onclickNumber={number => console.log(number)}/>
 
       <Functions onContentClear={onContentClearFunction} onDelete={onDeleteFunction}/>
 
