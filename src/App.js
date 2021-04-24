df4da924f82993d6fcf8be60f2a0977fdbb854cb
Rodrigo Hernandez <hernandez@eudaimonia.com.ar>
2021-04-24 00:29:38 -0300

    feat: use component mathOperations

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 
 import Result from './components/Result'
 import Button from './components/Button'
-
+import MathOperations from './components/MathOperations'
 //Style
 
 import './App.css';
@@ -14,25 +14,37 @@ const App = () => {
 
   //If the arrow function has just one parameter, is not needed the parenthesis
   const clickHandlerFunction = text => console.log(text)
-    
+
+  //mathOperations function values
+
+  const onClickOperationFunction = (operation) => {
+    console.log("clickOperation", operation)
+  }
+
+  const onClickEqualFunction = (equal) => {
+    console.log("EqualOperation", equal)
+
+  }
+
   return (
     <main className='react-calculator'>
 
       <Result value={"0"} />
 
       <div className="numbers">
-        <Button
-          text="1"
-          clickHandler={clickHandlerFunction} />
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>0</button>
+        <Button text="1" clickHandler={clickHandlerFunction} />
+        <Button text="2" clickHandler={clickHandlerFunction} />
+        <Button text="3" clickHandler={clickHandlerFunction} />
+        <Button text="4" clickHandler={clickHandlerFunction} />
+        <Button text="5" clickHandler={clickHandlerFunction} />
+        <Button text="6" clickHandler={clickHandlerFunction} />
+        <Button text="7" clickHandler={clickHandlerFunction} />
+        <Button text="8" clickHandler={clickHandlerFunction} />
+        <Button text="9" clickHandler={clickHandlerFunction} />
+        <Button text="0" clickHandler={clickHandlerFunction} />
+
+
+
       </div>
       <div className="functions">
         <button>
@@ -42,13 +54,7 @@ const App = () => {
           r
             </button>
       </div>
-      <div className="math-operations">
-        <button>+</button>
-        <button>-</button>
-        <button>*</button>
-        <button>/</button>
-        <button>=</button>
-      </div>
+      <MathOperations onClickOperation={onClickOperationFunction} onClickEqual={onClickEqualFunction} />
 
     </main>)
 }
