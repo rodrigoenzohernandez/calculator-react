4b79f5b409354bca150e785e4521c9e0f7b74f33
Rodrigo Hernandez <hernandez@eudaimonia.com.ar>
2021-04-27 21:00:46 -0300

    feature: add hook useState and create functionalities for the calculator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 //Installed components
 
+import React, { useState } from 'react'
+
 //Created components
 
 import Result from './components/Result'
@@ -13,32 +15,58 @@ import './App.css';
 
 const App = () => {
 
+  //useState devuelve un array con dos elementos (el state y la función que modifica ese state)
+
+
+  //Array destructuring
+
+  /* BEFORE
+  
+    const arrayStateFunction = useState("")
+  const state = arrayStateFunction[0]
+  const functionState = arrayStateFunction[1]
+
+  */
+
+  const [stack, setStack] = useState("");
+
+
   //mathOperations function values
 
   const onClickOperationFunction = (operation) => {
     console.log("clickOperation", operation)
+    setStack(`${stack}${operation}`)
+
   }
 
   const onClickEqualFunction = (equal) => {
     console.log("EqualOperation", equal)
+    setStack(eval(stack).toString())
+
   }
 
   //functions 
 
   const onContentClearFunction = () => {
+    setStack(' ')
     console.log("ClearOperation")
   }
 
   const onDeleteFunction = () => {
-    console.log("DeleteOperation")
+    if (stack.length > 0){
+      console.log("DeleteOperation")
+      const newStack = stack.substring(0, stack.length -1)
+      setStack(newStack)}
+
+
   }
 
   return (
     <main className='react-calculator'>
 
-      <Result value={"0"} />
+      <Result value={stack} />
 
-      <Numbers onclickNumber={number => console.log(number)}/>
+      <Numbers onclickNumber={number => setStack(`${stack}${number}`)}/>
 
       <Functions onContentClear={onContentClearFunction} onDelete={onDeleteFunction}/>
 
