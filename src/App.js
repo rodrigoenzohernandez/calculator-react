baa20881508175c59ee023a7d5ffa71461f6fca4
Rodrigo Hernandez <rodrigoenzohernandez@gmail.com>
2021-05-25 12:49:20 -0300

    lint and fix errors

--- src/App.js
+++ src/App.js
@@ -1,55 +1,68 @@
 //Installed components
-import React, { useState } from 'react'
-import words from 'lodash.words'
+import React, { useState } from "react";
+import words from "lodash.words";
 
 //Created components
-import Result from './components/Result'
-import MathOperations from './components/MathOperations'
-import Functions from './components/Functions'
-import Numbers from './components/Numbers'
+import Result from "./components/Result";
+import MathOperations from "./components/MathOperations";
+import Functions from "./components/Functions";
+import Numbers from "./components/Numbers";
 
 //Style
-import './App.css';
+import "./App.css";
 
 const App = () => {
-
   const [stack, setStack] = useState("");
-  const items = words(stack, /[^-^+^*^/]+/g)
+  const items = words(stack, /[^-^+^*^/]+/g);
+  const operaciones = ["-", "+", "*", "/"];
 
   const onClickOperationFunction = (operation) => {
-    setStack(`${stack}${operation}`)
-  }
+    //if las character on a string is an operator, is replaced with the new one
+    let lastPosition = stack[stack.length - 1];
+    if (operaciones.includes(lastPosition)) {
+      onDeleteFunction();
+    }
+    setStack(`${stack}${operation}`);
+  };
 
   const onClickEqualFunction = (equal) => {
-    // eslint-disable-next-line
-    setStack(eval(stack).toString())
-  }
+    let lastPosition = stack[stack.length - 1];
+    if (!operaciones.includes(lastPosition)) {
+      /*eslint no-eval: "off"*/
+      if(stack.length) setStack(eval(stack).toString());
+    }
+  };
 
   const onContentClearFunction = () => {
-    setStack(' ')
-  }
+    setStack(" ");
+  };
 
   const onDeleteFunction = () => {
     if (stack.length > 0) {
-      const newStack = stack.substring(0, stack.length - 1)
-      setStack(newStack)
+      const newStack = stack.substring(0, stack.length - 1);
+      setStack(newStack);
     }
-  }
+  };
 
-  const value = items.length > 0 ? items[items.length - 1] : "0"
+  const value = items.length > 0 ? items[items.length - 1] : "0";
 
   return (
-    <main className='react-calculator'>
-
+    <main className="react-calculator">
       <Result value={value} />
 
-      <Numbers onclickNumber={number => setStack(`${stack}${number}`)} />
+      <Numbers onclickNumber={(number) => setStack(`${stack}${number}`)} />
 
-      <Functions onContentClear={onContentClearFunction} onDelete={onDeleteFunction} />
+      <Functions
+        onContentClear={onContentClearFunction}
+        onDelete={onDeleteFunction}
+      />
 
-      <MathOperations onClickOperation={onClickOperationFunction} onClickEqual={onClickEqualFunction} />
-
-    </main>)
-}
+      <MathOperations
+        onClickOperation={onClickOperationFunction}
+        onClickEqual={onClickEqualFunction}
+      />
+    </main>
+  );
+};
 
 export default App;
